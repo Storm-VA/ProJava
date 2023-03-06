7b34b60cd40d7a569bf123130db3893ec1287d24
Storm-VA <116569545+Storm-VA@users.noreply.github.com>
2023-03-06 14:15:13 +0100

    rename animals

--- src/main/java/EgorovJavaPractic/Main.java
+++ src/main/java/EgorovJavaPractic/Main.java
@@ -4,19 +4,19 @@ package EgorovJavaPractic;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat = new Cat("Whiskers", 3, "meow");
+        Cat cat = new Cat("Kiss", 3, "meow");
         cat.makeSound();
         cat.eat();
         cat.sleep();
 
-        Dog dog = new Dog("Fido", 5, "woof");
+        Dog dog = new Dog("Gav", 5, "woof");
         dog.makeSound();
         dog.eat();
         dog.sleep();
 
-        Snake snake = new Snake("Slytherin", 2, "hiss");
+        Snake snake = new Snake("Snake2", 2, "hiss");
         snake.makeSound();
         snake.eat();
-        snake.sleep(); 
+        snake.sleep();
     }
 }
